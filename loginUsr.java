fc67da5af703b74af3a08c5acee37ccc4439a1a0
Jyot <jyo@gmail.com>
2024-07-06 19:22:40 +0530

    added string prgm

--- loginUsr.java
+++ loginUsr.java
@@ -1,4 +1,13 @@
-tfc 
-rf
-yuikjfd
-luygf
+class Main {
+  public static void main(String[] args) {
+
+    // create a string
+    String greet = "Hello! World";
+    System.out.println("String: " + greet);
+
+    // get the length of greet
+    int length = greet.length();
+
+    System.out.println("Length: " + length);
+  }
+}
